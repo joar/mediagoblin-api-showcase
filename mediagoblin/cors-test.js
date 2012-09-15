69a4efbf71262a851c2b168a064d4535e8d46acc
Joar Wandborg <git@wandborg.com>
2012-09-16 01:50:37 +0200

    Changed to work with the new API, added support for uploads

--- mediagoblin/cors-test.js
+++ mediagoblin/cors-test.js
@@ -1,6 +1,18 @@
+"use strict";
 var MediaGoblin = function (endpoint) {
     return MediaGoblin.init(endpoint);
 };
+if (! ('sendAsBinary' in new XMLHttpRequest())) {
+    console.log('Using sendAsBinary polyfill');
+    XMLHttpRequest.prototype.sendAsBinary = function(datastr) {
+        function byteValue(x) {
+            return x.charCodeAt(0) & 0xff;
+        }
+        var ords = Array.prototype.map.call(datastr, byteValue);
+        var ui8a = new Uint8Array(ords);
+        this.send(ui8a.buffer);
+    }
+}
 
 (function (mg) {
     mg.init = function (endpoint) {
@@ -25,62 +37,125 @@ var MediaGoblin = function (endpoint) {
         client.setRequestHeader('Accept', 'application/json');
         client.send()
     };
+    mg.post = function (url, params, cb) {
+        var client = new XMLHttpRequest();
+
+        client.addEventListener('readystatechange', function () {
+            if (this.readyState == this.DONE) {
+                if (this.status == 200) {
+                    cb(JSON.parse(this.responseText));
+                }
+            }
+        });
+
+        var boundary = 'MediaGoblinJavaScriptClient'
+            + new String(Math.round(Math.random() * 1000, 0));
+
+        client.open('POST', this.endpoint + url, true);
+        var body = '';
+
+        if (params.files && params.files.length) {
+            client.setRequestHeader('Content-Type',
+                    'multipart/form-data; boundary=----' + boundary);
+
+            for (var i in params.files) {
+                var file = params.files[i];
+                body += "------" + boundary + "\r\n";
+
+                body += 'Content-Disposition: form-data; name="'
+                    + file.name + '"; filename="'
+                    + file.filename + '"\r\n';
+                body += 'Content-Type: ' + file.type + '\r\n\r\n';
+                body += file.data + "\r\n";
+                body += "------" + boundary + "--";
+            }
+            client.sendAsBinary(body);
+        } else {
+            client.send(body);
+        }
+    };
 })(MediaGoblin);
 
-$(document).ready(function () {
-    mg = MediaGoblin('http://joar.pagekite.me');
+var mg = MediaGoblin('https://joar.pagekite.me');
 
-    $('#status').text('Loading...');
+document.getElementById('status').textContent = 'Loading...';
 
-    mg.get('/', function (data) {
-        $('#status').remove();
-        console.log('Response:', data);
-        for (i in data) {
-            $('#images').append('<li>'
-                    + '<img src="'
-                    + mg.endpoint + data[i].media_files.thumb
-                    + '" />'
-                    + '<br />'
-                    + '<p>'
-                    + data[i].title
-                    + '</p>'
-                    + '</li>');
-        }
-    });
+var fileField = document.getElementById('file');
+
+function onFileChange(e) {
+    readFileData(fileField);
+}
+
+function readFileData(fileField) {
+    var file = fileField.files[0];
+    var fr = new FileReader();
+    fr.onloadend = function (e) {
+        console.log(this.result.length);
+        uploadFile(fileField.name, this.result, file.type, file.name);
+        console.log('Done');
+    };
+    console.log('Reading data from ' + file.name);
+    fr.readAsBinaryString(file);
+}
+function uploadFile(name, data, type, filename) {
+    console.log('Uploading file...');
+    mg.post('/api/submit?access_token='
+            + document.getElementById('access_token').value, {
+        files: [
+        {
+            name: name,
+            data: data,
+            type: type,
+            filename: filename
+        }]},
+        function (response) {
+            console.log(response);
+        });
+}
+
+fileField.addEventListener('change', onFileChange);
+
+mg.get('/api/entries', function (data) {
+    document.body.removeChild(document.getElementById('status'));
+    console.log('Response:', data);
+    for (var i in data) {
+        var entry = data[i];
+        var em = document.createElement('li');
+
+        var action_em = document.createElement('p');
+        var user_link = document.createElement('a');
+
+
+        user_link.textContent = entry.user;
+        user_link.href = entry.user_permalink;
+
+        var action_desc = document.createElement('span');
+
+        action_desc.textContent = ' posted ';
+
+        var entry_title = document.createElement('a');
+        entry_title.href = entry.permalink;
+        entry_title.textContent = entry.title;
+
+        var thumbnail_link = document.createElement('a');
+        thumbnail_link.href = entry.permalink;
+
+        var thumbnail = document.createElement('img');
+        thumbnail.src = entry.media_files.thumb;
+
+        thumbnail_link.appendChild(thumbnail);
+
+        var time = document.createElement('p');
+        time.textContent = new Date(entry.created.replace(/\.[0-9]+$/, 'Z'));
+
+        action_em.appendChild(user_link);
+        action_em.appendChild(action_desc);
+        action_em.appendChild(entry_title);
+
+        em.appendChild(action_em);
+        em.appendChild(thumbnail_link);
+        em.appendChild(time);
+
+        document.getElementById('images').appendChild(em);
+    }
 });
-/*
-$(window).load(function(){
-$(document).ready(function () {
-    url = 'http://joar.pagekite.me/submit/';
-    $(document).delegate('#fileinput', 'change', function () {
-        console.log($(this));
-        var file = $(this)[0].files[0],
-            client = new XMLHttpRequest();
-        
-        
-        client.upload.addEventListener('progress', function (ev) {
-            if (ev.lengthComputable) {
-                $('#result').text('uploaded ' + ev.loaded + ' of total ' + ev.total);
-            }
-        }, false);
-        
-        client.upload.addEventListener('error', function (ev) {
-            console.log(ev);
-        }, false);
-        
-        client.onreadystatechange = function(){            
-            if (this.readyState == this.DONE){
-                console.log(this);
-            }
-        };
-        
-        client.open('POST', url, true);
-        
-        client.setRequestHeader('Accept', 'application/json');
-        client.setRequestHeader("Content-Type", "application/octet-stream");
-        client.send(file);
-        
-    });
-});
-});
-*/
